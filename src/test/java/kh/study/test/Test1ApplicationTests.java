5c4bd695b80e01396174660be309c6e7c79b42f0
zeejeezee <jeezeedev@gmail.com>
2022-10-27 10:10:57 +0900

    프로젝트 생성

---
+++ src/test/java/kh/study/test/Test1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.study.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

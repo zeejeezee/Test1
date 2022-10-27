42d8815bb2aaaab4ec052e5ab82bb2082401a75e
zeejeezee <jeezeedev@gmail.com>
2022-10-27 11:08:30 +0900

    집에서 작업한 첫 내용

--- src/main/java/kh/study/test/Test1Application.java
+++ src/main/java/kh/study/test/Test1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
 	public static void main(String[] args) {
+		//집에서 작업한 주석
 		SpringApplication.run(Test1Application.class, args);
 	}
 
